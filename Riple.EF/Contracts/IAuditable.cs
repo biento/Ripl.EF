29391486e714ce12f919825bcc37b1c75e005dd3
bien <toribiobien@gmail.com>
2020-04-12 23:39:48 +0800

    added IAuditable

---
+++ Riple.EF/Contracts/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ripl.EF.Contracts
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
+    }
+}
